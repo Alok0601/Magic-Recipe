aaee007e570140078c069255a7c479cd61772268
amal1kar <promatics.amal@gmail.com>
2020-04-12 20:37:21 +0530

    Initial Commit

---
+++ Magic Recipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Recipe
+//
+//  Created by Apple on 12/04/20.
+//  Copyright © 2020 Alok Ranjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
